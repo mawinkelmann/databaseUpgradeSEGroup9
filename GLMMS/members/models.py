21405bf12b13b16a5326eabcf0aac3dc6ba9027d
Jason Pulis <jbpulis@yahoo.com>
2018-10-28 20:03:30 -0500

    replacing the old bad code with the new code base
    the old code was junk, like bad junk. This should be a better foundation.

--- GLMMS/members/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-from django.urls import reverse # Used to generate URLs by reversing the URL patterns
-
-class Member(models.Model):
-    """Model representing a member."""
-    first_name = models.CharField(max_length=200)
-    last_name = models.CharField(max_length=200)
