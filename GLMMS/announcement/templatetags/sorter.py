c4d239d318230a7225bbe42bc91c227b465806ec
Jason Pulis <jbpulis@yahoo.com>
2018-11-06 14:18:51 -0600

    announcements update

---
+++ GLMMS/announcement/templatetags/sorter.py
@@ -0,0 +1,21 @@
+from urllib.parse import urlencode
+from collections import OrderedDict
+from django import template
+
+register = template.Library()
+
+@register.simple_tag
+def url_replace(request, field, value, direction=''):
+    dict_ = request.GET.copy()
+
+    if field == 'order_by' and field in dict_.keys():
+      if dict_[field].startswith('-') and dict_[field].lstrip('-') == value:
+        dict_[field] = value
+      elif dict_[field].lstrip('-') == value:
+        dict_[field] = "-" + value
+      else:
+        dict_[field] = direction + value
+    else:
+      dict_[field] = direction + value
+
+    return urlencode(OrderedDict(sorted(dict_.items())))
--- GLMMS/announcement/views.py
+++ GLMMS/announcement/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
-from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.http import HttpResponse, HttpRequest, Http404, HttpResponseRedirect
 from django.views import generic
 from django.utils import timezone
 from django import forms
@@ -9,30 +9,12 @@ from .models import Announcement
 
 # Create your views here.
 
-# def AnnouncementsView(request):
-#
-#     template_name = 'member/profile.html'
-#
-#     message=Announcement.message
-#
-#     # Number of visits to this view, as counted in the session variable.
-#     num_visits=request.session.get('num_visits', 0)
-#     request.session['num_visits'] = num_visits+1
-#
-#     return render(
-#         request,
-#         'announcement_view_all.html',
-#         context={
-#             'num_visits':num_visits,
-#             'message':message},
-#     )
-
 class AnnouncementsView(generic.ListView):
     model = Announcement
     template_name = 'announcement_view_all.html'
 
+    #order_by = request.GET.get('order_by', 'defaultOrderField')
+    #Model.objects.all().order_by(order_by)
+
     def get_queryset(self):
         return Announcement.objects.all()
-
-class DetailView(generic.DetailView):
-    model = Announcement
