b09b49031a0859902b1f629a329b30ee1407f8ea
Christopher Whetsel <cjwhetsel@gmail.com>
2018-11-08 14:12:15 -0600

    Bug fixes
    Tests pass now.

---
+++ GLMMS/member/forms.py
@@ -0,0 +1,63 @@
+from django import forms
+from django.contrib.auth.models import User
+
+from .models import Profile
+
+class SingleSearchForm(forms.Form):
+	'''Form class for the single search view'''
+	#get the data to populate the select forms with
+	#create the model fields
+	actives_choice = forms.ChoiceField(required=False, choices=[], label="Active Members")
+	alumni_choice = forms.ChoiceField(required=False, choices=[], label="Alumni Members")
+	newmembers_choice = forms.ChoiceField(required=False, choices=[], label="New Members")
+	inactives_choice = forms.ChoiceField(required=False, choices=[], label="Inactive Members")
+	def __init__(self, *args, **kwargs):
+		super(SingleSearchForm, self).__init__(*args, **kwargs)
+		BLANK='----'
+		members = Profile.get_all_members_by_category(BLANK)
+		self.fields['actives_choice'] = forms.ChoiceField(required=False, choices=members[Profile.ACTIVE], label="Active Members")
+		self.fields['alumni_choice'] = forms.ChoiceField(required=False, choices=members[Profile.ALUMNI], label="Alumni Members")
+		self.fields['newmembers_choice'] = forms.ChoiceField(required=False, choices=members[Profile.NEWMEMBER], label="New Members Members")
+		self.fields['inactives_choice'] = forms.ChoiceField(required=False, choices=members[Profile.INACTIVE], label="Inactive Members")
+	
+class MultiSearchForm(forms.Form):
+	'''Form class for the multi member search view'''
+	#get the data to populate the select forms with
+	#create the model fields
+	status_choice = forms.ChoiceField()
+	#fields = [(field.name, field.verbose_name) for field in Profile._meta.get_fields() if not isinstance(field, ManyToOneRel)]
+	field_choice = forms.MultipleChoiceField()
+	order_choice = forms.MultipleChoiceField()
+	ALL_MEMBERS = '__all__'
+	def __init__(self, *args, **kwargs):
+		super(MultiSearchForm, self).__init__(*args, **kwargs)
+		#fields to pick which collumns to search and how to sort
+		#not in ('bio', 'photo', 'jobs', 'organizations', 'positions', 'pledge_father', 'cell_carrier_id')]
+		dict_fields = Profile.get_printable_fields()
+		fields = []
+		for key, value in dict_fields.items():
+			fields.append((key, value))
+		statuses = list(Profile.STATUS)
+		statuses.insert(0, (MultiSearchForm.ALL_MEMBERS, 'All Members'))
+		self.fields['status_choice'] = forms.ChoiceField(required=True, choices=statuses, initial={MultiSearchForm.ALL_MEMBERS: 'Everything'}, label="Member Group to Search?")
+		#multiple choice fields are a multipe select
+		fields.insert(0,(MultiSearchForm.ALL_MEMBERS, 'Everything'))
+		self.fields['field_choice'] = forms.MultipleChoiceField(required=True, choices=fields, initial=MultiSearchForm.ALL_MEMBERS, label="What information to display?")
+		#remove everything option from the ordering cause that doesnt make sense
+		fields.remove((MultiSearchForm.ALL_MEMBERS, 'Everything'))
+		fields.insert(0,('user__username', 'Username'))
+		fields.insert(1,('user__last_name', 'Username'))
+		fields.insert(2,('user__first_name', 'Username'))
+		self.fields['order_choice'] = forms.MultipleChoiceField(required=True, choices=fields, initial='user__username', label="How to sort the results?")
+
+class ProfileEditForm(forms.ModelForm):
+	'''This class provides a profile edit form.'''
+	class Meta:
+		model = Profile
+		fields = ['bio', 'phone', 'address', 'pledge_class', 'major', 'major2', 'emphasis', 'grad_semester', 'grad_year', 'shirt_size', 'member_status', 'linkedin_profile', 'cumulative_gpa', 'last_sem_gpa', 'last_update', 'birth_date', 'photo', 'pledge_father', 'cell_carrier_id']
+
+class UserEditForm(forms.ModelForm):
+	'''THis class provides an edit form for the user.'''
+	class Meta:
+		model = User
+		fields = ['first_name', 'last_name', 'email']
--- GLMMS/member/views.py
+++ GLMMS/member/views.py
@@ -4,65 +4,19 @@ from django.urls import reverse
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.views import generic
 from django.utils import timezone
-from django import forms
 from django.contrib.auth.decorators import login_required
 #from django.template import loader 
 
 from django.contrib.auth.models import User
 from django.core import serializers
 from .models import Profile
+from .forms import ProfileEditForm, MultiSearchForm, SingleSearchForm, UserEditForm
 
 
 class ProfileDetailView(generic.DetailView):
 	'''This is a detail view for the a profile'''
 	model = Profile
 	template_name = 'member/profile.html'
-
-class SingleSearchForm(forms.Form):
-	'''Form class for the single search view'''
-	#get the data to populate the select forms with
-	#create the model fields
-	BLANK='----'
-	actives_choice = forms.ChoiceField(required=False, choices=Profile.get_all_members_by_category(BLANK)[Profile.ACTIVE], label="Active Members")
-	alumni_choice = forms.ChoiceField(required=False, choices=Profile.get_all_members_by_category(BLANK)[Profile.ALUMNI], label="Alumni Members")
-	newmembers_choice = forms.ChoiceField(required=False, choices=Profile.get_all_members_by_category(BLANK)[Profile.NEWMEMBER], label="New Members")
-	inactives_choice = forms.ChoiceField(required=False, choices=Profile.get_all_members_by_category(BLANK)[Profile.INACTIVE], label="Inactive Members")
-
-	
-class MultiSearchForm(forms.Form):
-	'''Form class for the multi member search view'''
-	#get the data to populate the select forms with
-	ALL_MEMBERS = '__all__'
-	#create the model fields
-	statuses = list(Profile.STATUS)
-	statuses.insert(0, (ALL_MEMBERS, 'All Members'))
-	status_choice = forms.ChoiceField(required=True, choices=statuses, label="Member Group to Search?")
-	#fields to pick which collumns to search and how to sort
-	#not in ('bio', 'photo', 'jobs', 'organizations', 'positions', 'pledge_father', 'cell_carrier_id')]
-	dict_fields = Profile.get_printable_fields()
-	#convert the fields to a list to use it in the choices field.
-	fields = []
-	for key, value in dict_fields.items():
-		fields.append((key, value))
-	#fields = [(field.name, field.verbose_name) for field in Profile._meta.get_fields() if not isinstance(field, ManyToOneRel)]
-	#multiple choice fields are a multipe select
-	fields.insert(0,(ALL_MEMBERS, 'Everything'))
-	field_choice = forms.MultipleChoiceField(required=True, choices=fields, label="What information to display?")
-	#remove everything option from the ordering cause that doesnt make sense
-	fields.remove((ALL_MEMBERS, 'Everything'))
-	order_choice = forms.MultipleChoiceField(required=True, choices=fields, label="How to sort the Results?")
-
-class ProfileEditForm(forms.ModelForm):
-	'''This class provides a profile edit form.'''
-	class Meta:
-		model = Profile
-		fields = ['bio', 'phone', 'address', 'pledge_class', 'major', 'major2', 'emphasis', 'grad_semester', 'grad_year', 'shirt_size', 'member_status', 'linkedin_profile', 'cumulative_gpa', 'last_sem_gpa', 'last_update', 'birth_date', 'photo', 'pledge_father', 'cell_carrier_id']
-
-class UserEditForm(forms.ModelForm):
-	'''THis class provides an edit form for the user.'''
-	class Meta:
-		model = User
-		fields = ['first_name', 'last_name', 'email']
 
 @login_required
 def search_index(request):
@@ -88,6 +42,7 @@ def single_search(request):
 					#we dont want to show the linked in profile in the table because its in the profile highlight area
 					del choices['linkedin_profile']
 			if profile is not None:
+				form = SingleSearchForm()
 				return render(request, 'member/single_search.html', {'form': form, 'profile': profile})
 			return render(request, 'member/single_search.html', {'form': form, 'error_message': "Please select a member from one of the lists!"})
     #handle a GET request for just the form
@@ -136,12 +91,9 @@ def multi_search(request):
 			print(order)
 			# get the requested subset of users
 			if status == MultiSearchForm.ALL_MEMBERS:
-				results = Profile.objects.all()
+				results = Profile.objects.all().order_by(*order)
 			else:
-				results = Profile.objects.filter(member_status=status)
-			#order the results
-			for x in order:
-				results.order_by(x)
+				results = Profile.objects.filter(member_status=status).order_by(*order)
 			#get the list of fields the user wanted to see. 
 			display_fields = []
 			for key in Profile.get_printable_fields().keys():
@@ -150,7 +102,7 @@ def multi_search(request):
 				else:
 					display_fields = fields
 			print(results)
-			print(display_fields)
+			#print(display_fields)
 			#have to serialize so we can loop through just these fields in the template 
 			data = serializers.serialize( "python", results, fields=display_fields)
 			#zip the profiles and the data to iterate over. could not figure out how to access specific items of the serialized object
