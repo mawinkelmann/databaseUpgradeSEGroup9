c0ac28c0cf50b3c84e9934b34912c41e02cdf014
Jason Pulis <jbpulis@yahoo.com>
2018-11-10 20:24:28 -0600

    added announcement detail view
    announcement detail view added; keyword: announcement_detail

--- GLMMS/announcement/urls.py
+++ GLMMS/announcement/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'announcement'
 urlpatterns = [
     path('announcement', views.AnnouncementsView, name='AnnouncementsView'),
+    path('announcement/<int:pk>/', views.announcement_detail, name='announcement_detail'),
 ]
--- GLMMS/announcement/views.py
+++ GLMMS/announcement/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
-from django.http import HttpResponse, HttpRequest, Http404, HttpResponseRedirect
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.views import generic
 from django.utils import timezone
 from django import forms
@@ -14,3 +14,8 @@ def AnnouncementsView(request):
 
     announcements = Announcement.objects.order_by('-dateAdded')
     return render(request, 'announcement/announcement_view_all.html', {'announcements': announcements})
+
+def announcement_detail(request, pk):
+
+    announcement = get_object_or_404(Announcement, pk=pk)
+    return render(request, 'announcement/announcement_detail.html', {'announcement': announcement})
