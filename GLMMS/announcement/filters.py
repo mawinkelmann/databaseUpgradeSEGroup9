136e4a21ea0f3b656e0293c2da98ab6414b9db02
Jason Pulis <jbpulis@yahoo.com>
2018-12-04 19:56:20 -0600

    change search to first name

--- GLMMS/announcement/filters.py
+++ GLMMS/announcement/filters.py
@@ -3,7 +3,7 @@ import django_filters
 from announcement.models import Announcement
 
 class AnnouncementFilter(django_filters.FilterSet):
-    creator = django_filters.CharFilter(lookup_expr='username__icontains')
+    creator = django_filters.CharFilter(lookup_expr='first_name__icontains')
     type = django_filters.ChoiceFilter(choices=Announcement.TYPES)
     topic = django_filters.ChoiceFilter(choices=Announcement.TOPICS)
     message = django_filters.CharFilter(lookup_expr='icontains')
